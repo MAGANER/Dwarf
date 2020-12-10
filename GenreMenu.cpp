1c9a721497d20d26a451f3377750ca28f8c550b5
MAGANER <maganer.krik@mail.ru>
2020-12-10 12:16:50 +0300

    fixed bug, when there was memory segmentation fault, when there were no any genre data

--- GenreMenu.cpp
+++ GenreMenu.cpp
@@ -17,6 +17,7 @@ void GenreMenu::run_list_genres(const vector<MusicData*>& music)
 	int start_counter = 0;
 	int max_counter   = genres.size()< visible_range?genres.size():visible_range;
 	
+	
 	while(true)
 	{
 		run_common_list(genres,
@@ -29,12 +30,12 @@ void GenreMenu::run_list_genres(const vector<MusicData*>& music)
 		if(choosen_option == -2) break;
 		if(choosen_option == current_elem)
 		{
-			system("cls");
+			clear();
 			choose_what_to_run_from_genre_menu(music,genres[choosen_option]);
 			choosen_option = -1;
 		}
 	}
-	system("cls");
+	clear();
 }
 void GenreMenu::choose_what_to_run_from_genre_menu(const vector<MusicData*>& music,
 												   const wstring& genre_name)
@@ -59,7 +60,7 @@ void GenreMenu::choose_what_to_run_from_genre_menu(const vector<MusicData*>& mus
 		if(choosen_option == 3) run_list_titles(music,genre_name,L"",L"");
 		if(choosen_option == -2) break;
 	}
-	system("cls");
+	clear();
 }
 wsvector GenreMenu::get_genre_data_from_music(const vector<MusicData*>& music)
 {
@@ -69,6 +70,6 @@ wsvector GenreMenu::get_genre_data_from_music(const vector<MusicData*>& music)
 		bool already_added = find(data.begin(),data.end(),m->genre) != data.end();
 		if(!already_added) data.push_back(m->genre);
 	}
-	data.erase(--data.end());
+	if(!data.empty())data.erase(--data.end());
 	return data;
 }
